43627ba5f1a6a23bcdb3d4c3a60d1af92ceda729
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-04-15 00:56:25 +0530

    integration done

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+import streamlit as st
+
+def run_streamlit_app(app_file, port):
+    subprocess.Popen(["streamlit", "run", app_file, "--server.port", str(port)])
+
+def main():
+    st.title("TEXT-IMAGE-TEXT")
+    st.markdown("---")
+
+    # Add two buttons for TEXT-IMAGE and IMAGE-TEXT
+    if st.button("TEXT-IMAGE", key="text_image_button"):
+        run_streamlit_app("text_image_retrieval.py", 8050)
+    
+    if st.button("IMAGE-TEXT", key="image_text_button"):
+        run_streamlit_app("app.py", 8051)
+
+if __name__ == "__main__":
+    main()
